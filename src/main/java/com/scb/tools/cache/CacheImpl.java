bba65fc0cceb0083f802e9979dbed6223c72a452
raphprz <85506967+raphprz@users.noreply.github.com>
2021-07-02 08:23:16 +0800

    Use a CacheEntry object to avoid the usage of a separate lock map

--- src/main/java/com/scb/tools/cache/CacheImpl.java
+++ src/main/java/com/scb/tools/cache/CacheImpl.java
@@ -8,11 +8,9 @@ import java.util.function.Function;
 
 public class CacheImpl<K, V> implements Cache<K, V> {
 
-    private final Map<Optional<K>, Optional<V>> cache = new ConcurrentHashMap<>();
+    private final Map<Optional<K>, CacheEntry<V>> cache = new ConcurrentHashMap<>();
     private final Function<K, V> valueLoader;
-    // holds the lock objects per key
-    private final Map<Optional<K>, ReentrantLock> locks = new ConcurrentHashMap<>();
-    // mutex used to create lock objects per key
+    // mutex used to create new CacheEntry objects
     private final ReentrantLock lock = new ReentrantLock();
 
     public CacheImpl(Function<K, V> valueLoader) {
@@ -26,24 +24,24 @@ public class CacheImpl<K, V> implements Cache<K, V> {
     public V get(K key) {
         Optional<K> optionalKey = Optional.ofNullable(key);
 
-        // First we need to create a lock object for this key, if it does not exist.
+        // First we need to create a CacheEntry object for this key, if it does not exist.
         // Because we want to avoid using putIfAbsent/computeIfAbsent, we have to use a common lock object here
         lock.lock();
         try {
-            if (!locks.containsKey(optionalKey))
-                locks.put(optionalKey, new ReentrantLock());
+            CacheEntry<V> current = cache.get(optionalKey);
+            if (current == null)
+                cache.put(optionalKey, new CacheEntry<>());
         } finally {
             lock.unlock();
         }
 
         // Now we can lock on a specific key
-        ReentrantLock lockOnKey = locks.get(optionalKey);
-        lockOnKey.lock();
-        try {
-            if (!cache.containsKey(optionalKey)) {
-                Optional<V> value;
+        synchronized (cache.get(optionalKey)) {
+            CacheEntry<V> current = cache.get(optionalKey);
+            if (!current.isComputed()) {
                 try {
-                    value = Optional.ofNullable(valueLoader.apply(key));
+                    current.setValue(valueLoader.apply(key));
+                    current.setComputed(true);
                 } catch (Exception e) {
                     // It can be useful here to have this line of log, to make it more obvious for the user
                     // that the error comes from within the value loader
@@ -51,14 +49,38 @@ public class CacheImpl<K, V> implements Cache<K, V> {
                     // but after logging, we rethrow to let the user see it and fix it
                     throw e;
                 }
-
-                cache.put(optionalKey, value);
             }
-        } finally {
-            lockOnKey.unlock();
         }
 
-        return cache.get(optionalKey).orElse(null);
+        return cache.get(optionalKey).getValue();
+    }
+
+    private static class CacheEntry<V> {
+
+        private V value;
+        private boolean computed;
+
+        public CacheEntry() {
+            this.value = null;
+            this.computed = false;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+        public boolean isComputed() {
+            return computed;
+        }
+
+        public void setComputed(boolean computed) {
+            this.computed = computed;
+        }
+
     }
 
 }
